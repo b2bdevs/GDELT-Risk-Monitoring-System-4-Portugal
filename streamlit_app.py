002491f39b202807b1a37c5a00cb7b4a04a57f32
b2devs <110261146+b2bdevs@users.noreply.github.com>
2024-07-06 17:57:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 # ---------------------------------------------
 # Load Modules
 
