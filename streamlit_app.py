0403dd010c375040c74e05a0568b4286851b56e4
b2devs <110261146+b2bdevs@users.noreply.github.com>
2024-07-06 17:36:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,20 +13,12 @@ import io
 from datetime import timedelta, datetime
 
 # ---------------------------------------------
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
+hide_github_icon = “”"
 
-#MainMenu {
-  visibility: hidden;
-}
-
-
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+“”"
 st.markdown(hide_github_icon, unsafe_allow_html=True)
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 
 # Initialize Settings
 
