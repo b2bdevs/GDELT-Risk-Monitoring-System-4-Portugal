ca2a7622a013eaa3ac7ffe5d69d910e44409db77
b2devs <110261146+b2bdevs@users.noreply.github.com>
2024-07-06 17:43:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,20 +13,6 @@ import io
 from datetime import timedelta, datetime
 
 # ---------------------------------------------
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-
 # Initialize Settings
 
 st.set_page_config(layout="wide")
